0aa3efa50bba2d4ff97830c5ad7f6942d737eacc
semihYusufq <semihyusufugdur@gmail.com>
2024-05-29 13:57:47 +0300

    first commit

---
+++ 1_1.000.000_Arasındaki_Asal_Sayıları_Listeleme.cs
@@ -0,0 +1,40 @@
+ 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int kontrol = 0;
+            int sayac = 0;
+            for (int i = 2; i <= 1000000; i++)
+            {
+                kontrol = 0;
+               
+                for (int j = 2; j <= i/2; j++)
+                {
+                    if(i%j==0)
+                    {
+                        kontrol++;
+                        break;
+                    }
+                }
+                if (kontrol == 0)
+                {
+                    sayac++;
+                    Console.Write(i+"-");
+                }
+                
+            }
+            Console.WriteLine();
+            Console.WriteLine("{0} adet asal sayı bulundu.", sayac);
+            Console.ReadKey();
+        }
+    }
+}
